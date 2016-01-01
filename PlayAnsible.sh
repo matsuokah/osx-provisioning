9b9f9be23a215c9272d8a036a32f16f99accd067
Hideki Matsuoka <matsuoka.hide@gmail.com>
2016-01-01 16:51:32 +0900

    Add defaults

--- PlayAnsible.sh
+++ PlayAnsible.sh
@@ -5,4 +5,15 @@ show_context
 
 cd ${MYSTAGE_HOME}/provisioning
 
-HOMEBREW_CASK_OPTS="--appdir=/Applications" ansible-playbook playbook.yml -i hosts -K
+ANSIBLE_VERSION_MAJOR=$(echo $(ansible --version) | sed -E "s/.*[^0-9]([0-9]+\.[0-9]+\.[0-9]+).*/\1/g" | cut -d"." -f1)
+
+if [ $ANSIBLE_VERSION_MAJOR -eq 1 ]
+then
+  PLAYBOOK=playbook_v1.yml
+else
+  PLAYBOOK=playbook.yml
+fi
+
+echo doing playbook is ... $PLAYBOOK
+
+HOMEBREW_CASK_OPTS="--appdir=/Applications" ansible-playbook ${PLAYBOOK} -i hosts -K
