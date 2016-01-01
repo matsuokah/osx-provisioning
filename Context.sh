fa5ee8d95ede9e54e8ac597e022e9e38a4ad39c0
Hideki Matsuoka <matsuoka.hide@gmail.com>
2016-01-01 18:04:17 +0900

    Move ansible version extracting from PlayAnsible.sh to Context.sh

--- Context.sh
+++ Context.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-MYSTAGE_HOME=$(cd $(dirname $0) && pwd)
+PROVISIONING_HOME=$(cd $(dirname $0) && pwd)
+ANSIBLE_VERSION_MAJOR=$(echo $(ansible --version) | sed -E "s/.*[^0-9]([0-9]+\.[0-9]+\.[0-9]+).*/\1/g" | cut -d"." -f1)
 
 function show_context() {
-  echo "MYSTAGE_HOME=${MYSTAGE_HOME}"
+  echo "PROVISIONING_HOME=${PROVISIONING_HOME}"
+  echo "ANSIBLE_VERSION_MAJOR=${ANSIBLE_VERSION_MAJOR}"
 }
--- PlayAnsible.sh
+++ PlayAnsible.sh
@@ -8,14 +8,12 @@ function pa_echo () {
   echo [${TASK_NAME}] $1
 }
 
-cd ${MYSTAGE_HOME}/provisioning > /dev/null 2>&1
+cd ${PROVISIONING_HOME}/provisioning > /dev/null 2>&1
 
 pa_echo "Doing common playbook"
 HOMEBREW_CASK_OPTS="--appdir=/Applications" ansible-playbook playbook.yml -i hosts
 
 
-pa_echo "Extract ansible major version..."
-ANSIBLE_VERSION_MAJOR=$(echo $(ansible --version) | sed -E "s/.*[^0-9]([0-9]+\.[0-9]+\.[0-9]+).*/\1/g" | cut -d"." -f1)
 if [ $ANSIBLE_VERSION_MAJOR -eq 1 ]
 then
   DEFAULTS_PLAYBOOK=playbook_defaults_v1.yml
@@ -23,6 +21,5 @@ else
   DEFAULTS_PLAYBOOK=playbook_defaults.yml
 fi
 
-pa_echo "Ansible major version is $ANSIBLE_VERSION_MAJOR"
 pa_echo "Doing playbook is ${DEFAULTS_PLAYBOOK}, which is depending ansible version"
 ansible-playbook ${DEFAULTS_PLAYBOOK} -i hosts
